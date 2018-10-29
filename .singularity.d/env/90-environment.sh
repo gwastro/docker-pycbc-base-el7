d668f699c495102c12f22ce7d155fcb097b978d7
Duncan Brown <dabrown@syr.edu>
2018-10-29 15:31:07 -0400

    more robust env script

--- .singularity.d/env/90-environment.sh
+++ .singularity.d/env/90-environment.sh
@@ -1,10 +1,15 @@
 # Custom environment shell code should follow
 
-# Source the PyCBC virtual environment
-if [ -f /opt/pycbc/pycbc-software/bin/activate ] ; then
+# Source the PyCBC virtual environment, but prevent recursive sourcing
+if [ x"$VIRTUAL_ENV" == "x" ]; then
+  if [ -f /opt/pycbc/pycbc-software/bin/activate ] ; then
     source /opt/pycbc/pycbc-software/bin/activate
+  else
+    echo "WARNING: Could not set up PyCBC virtual environment"
+  fi
 fi
 
-# Use the lal-data bundled in the image
-LAL_DATA_PATH=/opt/pycbc/pycbc-software/opt/lalsuite/share/lalsimulation
-export LAL_DATA_PATH
+# If no LAL_DATA_PATH has been set, use the data in the container
+if [ "x$LAL_DATA_PATH" == "x" ]; then
+    export LAL_DATA_PATH="/opt/pycbc/pycbc-software/opt/lalsuite/share/lalsimulation"
+fi
