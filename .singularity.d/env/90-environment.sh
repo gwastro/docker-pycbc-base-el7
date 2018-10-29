620d6d37395cc33677ec9b64dbed4330cc4afe42
Duncan Brown <dabrown@syr.edu>
2018-10-29 14:56:28 -0400

    set the LAL_DATA_PATH

--- .singularity.d/env/90-environment.sh
+++ .singularity.d/env/90-environment.sh
@@ -4,3 +4,7 @@
 if [ -f /opt/pycbc/pycbc-software/bin/activate ] ; then
     source /opt/pycbc/pycbc-software/bin/activate
 fi
+
+# Use the lal-data bundled in the image
+LAL_DATA_PATH=/opt/pycbc/pycbc-software/opt/lalsuite/share/lalsimulation
+export LAL_DATA_PATH
