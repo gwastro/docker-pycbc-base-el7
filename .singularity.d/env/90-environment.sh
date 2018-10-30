e1af7dba278028e45a6c812d01694f4da5cbf258
Duncan Brown <dabrown@syr.edu>
2018-10-30 10:35:21 -0400

    deactivate any existing virtual environment

--- .singularity.d/env/90-environment.sh
+++ .singularity.d/env/90-environment.sh
@@ -1,12 +1,16 @@
 # Custom environment shell code should follow
 
-# Source the PyCBC virtual environment, but prevent recursive sourcing
-if [ x"$VIRTUAL_ENV" == "x" ]; then
-  if [ -f /opt/pycbc/pycbc-software/bin/activate ] ; then
-    source /opt/pycbc/pycbc-software/bin/activate
-  else
-    echo "WARNING: Could not set up PyCBC virtual environment"
-  fi
+# Deactivate any existing virtual environment
+if [ x"$VIRTUAL_ENV" != "x" ]; then
+   echo "WARNING: Already in virtual environment, deactivating $VIRTUAL_ENV"
+   deactivate
+fi
+
+# Source the PyCBC virtual environment
+if [ -f /opt/pycbc/pycbc-software/bin/activate ] ; then
+  source /opt/pycbc/pycbc-software/bin/activate
+else
+  echo "WARNING: Could not set up PyCBC virtual environment"
 fi
 
 # If no LAL_DATA_PATH has been set, use the data in the container
