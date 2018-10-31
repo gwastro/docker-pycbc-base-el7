6346c17cffeeeff2321f8faa766b848be79bcd94
Duncan Brown <dabrown@syr.edu>
2018-10-31 09:03:11 -0400

    prevent double sourcing in singularity and add mkl

--- etc/profile.d/pycbc.sh
+++ etc/profile.d/pycbc.sh
@@ -1,8 +1,22 @@
-# Source the PyCBC virtual environment
-if [ -f /opt/pycbc/pycbc-software/bin/activate ] ; then
-    source /opt/pycbc/pycbc-software/bin/activate
-fi
+# Singularity already sets up the login environment
+if [ "x${SINGULARITY_CONTAINER}" == "x" ] ; then
 
-# Use the lal-data bundled in the image
-LAL_DATA_PATH=/opt/pycbc/pycbc-software/opt/lalsuite/share/lalsimulation
-export LAL_DATA_PATH
+    # Source the PyCBC virtual environment
+    if [ -f /opt/pycbc/pycbc-software/bin/activate ] ; then
+        source /opt/pycbc/pycbc-software/bin/activate
+    fi
+
+    # Use the lal-data bundled in the image if not set
+    if [ "x${LAL_DATA_PATH}" == "x" ]
+        LAL_DATA_PATH=/opt/pycbc/pycbc-software/opt/lalsuite/share/lalsimulation
+        export LAL_DATA_PATH
+    fi
+
+    # Add path to Intel MKL Libraries
+    if [ "x$LD_LIBRARY_PATH" == "x" ]; then
+        LD_LIBRARY_PATH="/opt/intel/composer_xe_2015.0.090/compiler/lib/intel64"
+    else
+        LD_LIBRARY_PATH="/opt/intel/composer_xe_2015.0.090/compiler/lib/intel64:${LD_LIBRARY_PATH}"
+    fi
+    export LD_LIBRARY_PATH
+fi
